543b767607ce0053680620b269a68e87252919f2
Zaki Abdulle <zakiabdulle1@gmail.com>
2021-08-15 23:37:03 +0100

    removed d3

--- client/src/components/WeatherTable.tsx
+++ client/src/components/WeatherTable.tsx
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import * as d3 from 'd3';
 
 // GET Element by text, that matches with the day && time onclick
 
